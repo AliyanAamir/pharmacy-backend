60c0de01bd22da6ba393f2bd41a1b0a3092d1923
Mohammad Aliyan Aamir <104263411+AliyanAamir@users.noreply.github.com>
2024-11-25 15:02:11 +0500

    Initial commit

---
+++ src/modules/auth/auth.router.ts
@@ -0,0 +1,56 @@
+import { canAccess } from '../../middlewares/can-access.middleware';
+import MagicRouter from '../../openapi/magic-router';
+import {
+  handleChangePassword,
+  handleForgetPassword,
+  handleGetCurrentUser,
+  handleGoogleCallback,
+  handleGoogleLogin,
+  handleLoginByEmail,
+  handleLogout,
+  handleResetPassword
+} from './auth.controller';
+import {
+  changePasswordSchema,
+  forgetPasswordSchema,
+  loginUserByEmailSchema,
+  resetPasswordSchema
+} from './auth.schema';
+
+export const AUTH_ROUTER_ROOT = '/auth';
+
+const authRouter = new MagicRouter(AUTH_ROUTER_ROOT);
+
+authRouter.post(
+  '/login/email',
+  { requestType: { body: loginUserByEmailSchema } },
+  handleLoginByEmail,
+);
+
+authRouter.post('/logout', {}, handleLogout);
+
+authRouter.get('/user', {}, canAccess(), handleGetCurrentUser);
+
+authRouter.post(
+  '/forget-password',
+  { requestType: { body: forgetPasswordSchema } },
+  handleForgetPassword,
+);
+
+authRouter.post(
+  '/change-password',
+  { requestType: { body: changePasswordSchema } },
+  canAccess(),
+  handleChangePassword,
+);
+
+authRouter.post(
+  '/reset-password',
+  { requestType: { body: resetPasswordSchema } },
+  handleResetPassword,
+);
+
+authRouter.get('/google', {}, handleGoogleLogin);
+authRouter.get('/google/callback', {}, handleGoogleCallback);
+
+export default authRouter.getRouter();
